382e03e316747fcc154cca38b3c4efbf355ff702
Riley Brown <ribrown@bishs.com>
2023-01-03 15:31:42 -0700

    Setting up for easy repro

--- apps/host/module-federation.config.js
+++ apps/host/module-federation.config.js
@@ -1,4 +1,13 @@
+// @ts-check
+
+/**
+ * @type {import('@nrwl/devkit').ModuleFederationConfig}
+ **/
 module.exports = {
   name: 'host',
-  remotes: ['about', 'cart', 'shop'],
+  remotes: [
+    ['about', 'http://192.168.0.243:4201'],
+    ['cart', 'http://192.168.0.243:4202'],
+    ['shop', 'http://192.168.0.243:4203'],
+  ],
 };
